470f229fa061cbf425ea45e4ab27a94c5022f66c
Josh Pospisil <josh.pospisil@gmail.com>
2018-06-18 08:24:52 -0500

    Renamed main application class

--- src/main/java/com/spring/api/Application.java
+++ src/main/java/com/spring/api/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringTestApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringTestApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
--- src/test/java/com/spring/api/ApplicationTests.java
+++ src/test/java/com/spring/api/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.spring.test;
+package com.spring.api;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringTestApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
