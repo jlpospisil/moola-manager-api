e1bf1efa43cea70b2d291394f2db8ef1f80e3f95
Josh Pospisil <josh.pospisil@gmail.com>
2018-06-19 16:18:57 -0500

    Removed unnecessary parameter

--- src/main/java/com/spring/api/authentication/JWTAuthenticationFilter.java
+++ src/main/java/com/spring/api/authentication/JWTAuthenticationFilter.java
@@ -2,14 +2,17 @@ package com.spring.api.authentication;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.spring.api.model.ApplicationUser;
+import com.spring.api.repository.UserRepository;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -17,6 +20,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
+
 import static com.spring.api.authentication.JWTConstants.EXPIRATION_TIME;
 import static com.spring.api.authentication.JWTConstants.SECRET;
 
@@ -33,12 +37,12 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
             ApplicationUser credentials = new ObjectMapper().readValue(request.getInputStream(), ApplicationUser.class);
 
             return authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                    credentials.getUsername(),
-                    credentials.getPassword(),
-                    new ArrayList<>()
-                )
+                    new UsernamePasswordAuthenticationToken(
+                            credentials.getUsername(),
+                            credentials.getPassword()
+                    )
             );
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -47,10 +51,10 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
         String token = Jwts.builder()
-            .setSubject(((User) authResult.getPrincipal()).getUsername())
-            .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-            .signWith(SignatureAlgorithm.HS512, SECRET.getBytes())
-            .compact();
+                .setSubject(((User) authResult.getPrincipal()).getUsername())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS512, SECRET.getBytes())
+                .compact();
 
         response.addHeader("Content-Type", "application/json");
 
