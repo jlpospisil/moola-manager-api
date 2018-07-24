8b38561e965cf3ed1cce449bb44e9c16f48cecf4
Josh Pospisil <josh.pospisil@gmail.com>
2018-07-24 15:56:09 -0500

    Added balance to account

--- src/main/java/com/spring/api/model/Account.java
+++ src/main/java/com/spring/api/model/Account.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,6 +34,11 @@ public class Account {
     @Getter
     @Setter
     private String description;
+
+    @NotNull
+    @Getter
+    @Setter
+    private BigDecimal balance = new BigDecimal("0.00");
 
     @Getter
     @Setter
